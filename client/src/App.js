e168683b86ccd1482c81a2fddb8aeb74baae854e
adkhiker <poirier.greg@gmail.com>
2019-12-12 21:18:08 -0500

    WIP: Added route to path

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from './Movies/UpdateMovie';
+import NewMovie from './Movies/NewMovie';
+import axios from "axios";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -10,6 +13,22 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
+
+  const movies = axios.get("http://localhost:5000/api/movies");
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => {
+        res.data.forEach(movie => {
+          savedList.forEach(sMovie => {
+            if(movie.id === sMovie.id) {
+             const newSavedList = savedList.filter(savedMovie => savedMovie !== sMovie)
+              setSavedList([...newSavedList, movie])
+            } 
+          })
+        })
+      })
+  }, [movies])
 
   return (
     <>
@@ -20,6 +39,14 @@ const App = () => {
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
+      />
+      <Route
+      path='/update-movie/:id'
+      component={UpdateMovie}
+      />
+      <Route
+      path='/new-movie'
+      component={NewMovie}
       />
     </>
   );
